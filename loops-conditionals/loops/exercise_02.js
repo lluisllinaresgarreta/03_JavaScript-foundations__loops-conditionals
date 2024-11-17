a138f3472a9555cdfca5678f0a35bec353662648
LLG <lluisllinares@gmail.com>
2024-11-17 20:39:10 +0100

    feat(challenge): Work on JavaScript practice tasks and connect the scripts to an HTML file

--- loops-conditionals/loops/exercise_02.js
+++ loops-conditionals/loops/exercise_02.js
@@ -18,3 +18,14 @@
     You made it!
     All done!
 */
+
+for (let i = 1; i <= 100; i++) {
+    if (i === 50) {
+        console.log("Halfway there!");
+    } else if (i === 100) {
+        console.log("You made it!");
+    } else if (i % 10 === 0) {
+        console.log(`Checkpoint! ${i}`);
+    }
+}
+console.log("All done!");
